c11d296aa8b213c3e7c21316590689b8d384ea82
Simon Søborg <s173255@student.dtu.dk>
2021-04-06 08:24:58 +0200

    Player and Clubs

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary;
+using DataLibrary.Logic;
+using static DataLibrary.Logic.PlayerProcessor;
+using Microsoft.Extensions.Configuration;
+using StatTrack.Models;
+
+namespace StatTrack.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var data = LoadPlayers();
+            List<PlayerModel> players = new List<PlayerModel>();
+
+            foreach (var item in data)
+            {
+                players.Add(new PlayerModel()
+                {
+                    Id = item.Id,
+                    PlayerName = item.PlayerName,
+                    PlayerPosition = item.PlayerPosition,
+                    YOB = item.PlayerYOB
+                });
+            }
+
+            return View(players);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
