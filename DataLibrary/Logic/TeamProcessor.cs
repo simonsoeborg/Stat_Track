750387a0973ccb7c5a00a681c9a08c2a4642cf97
SimonF <s195479@student.dtu.dk>
2021-04-19 12:01:26 +0200

    Initial update of datalibary conerning Team.

---
+++ DataLibrary/Logic/TeamProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLibrary.DataAccess;
+using DataLibrary.Models;
+
+namespace DataLibrary.Logic
+{
+    class TeamProcessor
+    {
+        public static int CreateTeam(string teamName, ClubModel club, string teamUYear, List<PlayerModel> playerList, int startYear, int endYear)
+        {
+            TeamModel data = new TeamModel()
+            {
+                Name = teamName,
+                Club = club,
+                Year = teamUYear,
+                PlayerList = playerList,
+                StartYear = startYear,
+                EndYear = endYear
+            };
+
+            int ClubId = club.Id; 
+
+            string query = @"INSERT INTO Team (Name, ClubId, TeamUYear, playerId, startDate, endDate) VALUES (@Name, @Position, @YOB);";
+
+            return SqlDataAccess.SaveData(query, data);
+        }
+
+    }
+}
---
+++ DataLibrary/Models/TeamModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.Models
+{
+    class TeamModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public ClubModel Club { get; set; }
+        public string Year { get; set; }
+        public string League { get; set; }
+        public List<PlayerModel> PlayerList { get; set; }
+
+        public int StartYear { get; set; }
+        public int EndYear { get; set; }
+
+    }
+}
